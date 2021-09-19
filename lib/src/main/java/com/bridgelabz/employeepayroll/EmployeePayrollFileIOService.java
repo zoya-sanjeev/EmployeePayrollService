b7e093d75f72052946fceacb3c99e9e1717b0dc8
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-19 22:00:45 +0530

    ADDED: printData function

--- lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollFileIOService.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollFileIOService.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.employeepayroll;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
@@ -19,6 +20,14 @@ public class EmployeePayrollFileIOService {
 			Files.write(Paths.get(PAYROLL_FILE_NAME),empBuffer.toString().getBytes());
 		}catch(IOException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void printData() {
+		try {
+			Files.lines(new File(PAYROLL_FILE_NAME).toPath()).forEach(System.out::println);
+		} catch(IOException e) {
+			e.printStackTrace(); 
 		}
 	}
 	
