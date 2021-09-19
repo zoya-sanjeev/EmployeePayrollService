3174b6d1852cd8cdcecc6d41e329bb0539cfa11d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-19 15:41:11 +0530

    ADDED: function to read employee payroll data

--- lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -1,8 +1,29 @@
 package com.bridgelabz.employeepayroll;
 
-public class EmployeePayrollService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class EmployeePayrollService {
+	
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService() {
+		this.employeePayrollList=new ArrayList<EmployeePayrollData>();
+	}
+
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID");
+		int id=consoleInputReader.nextInt();
+		System.out.println("Enter employee name");
+		String name=consoleInputReader.next();
+		System.out.println("Enter Employee Salary");
+		Double salary =consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
 	public static void main(String[] args) {
+		EmployeePayrollService employeePayrollService= new EmployeePayrollService();
+		Scanner consoleInputReader=new Scanner(System.in);
 		
 
 	}
