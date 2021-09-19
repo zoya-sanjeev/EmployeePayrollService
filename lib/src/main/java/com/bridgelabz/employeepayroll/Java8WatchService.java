4bb17c3bc279d84f2dfb4d52ef4e9f722248d19a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-19 19:40:52 +0530

    ADDED: function to process Events

--- lib/src/main/java/com/bridgelabz/employeepayroll/Java8WatchService.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/Java8WatchService.java
@@ -11,6 +11,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +36,39 @@ public class Java8WatchService {
 				return FileVisitResult.CONTINUE;
 			}
 		});
+	}
+	
+	void processEvents() {
+		while(true) {
+			WatchKey key;
+			try {
+				key=watcher.take();
+			} catch(InterruptedException x) {
+				return;
+			}
+			Path dir = dirWatchers.get(key);
+			if(dir==null) 	continue;
+			for(WatchEvent<?> event : key.pollEvents()) {
+				WatchEvent.Kind kind = event.kind();
+				Path name = ((WatchEvent<Path>)event).context();
+				Path child = dir.resolve(name);
+				System.out.format("%s: %s\n",event.kind().name(),child);
+				
+				if(kind == StandardWatchEventKinds.ENTRY_CREATE) {
+					try {
+						if(Files.isDirectory(child)) scanAndRegisterDirectories(child);
+					} catch(IOException x) {}
+				} else if(kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+					if(Files.isDirectory(child)) dirWatchers.remove(key);
+				}
+			}
+			
+			boolean valid = key.reset();
+			if(!valid) {
+				dirWatchers.remove(key);
+				if(dirWatchers.isEmpty()) break;
+			}
+		}
 	}
 
 }
