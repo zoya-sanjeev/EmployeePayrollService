3468d29a386c9704246da5a503811414bf4c629c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-19 11:58:16 +0530

    ADDED: class variables and constructor to EmployeePayrollService

--- lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
@@ -4,5 +4,17 @@
 package com.bridgelabz.employeepayroll;
 
 public class EmployeePayrollData {
+	public int id;
+	public String name;
+	public double salary;
 	
+	public EmployeePayrollData(int id, String name, Double salary) {
+		this.id=id;
+		this.name=name;
+		this.salary=salary;
+	}
+	
+	public String toString() {
+		return "id=" + id + ", name=\'"+ name + "\'" + "salary="+ salary;
+	}
 }
