92b5cad21da8f6effd44a0b5fa28adfd9c103753
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-25 11:39:15 +0530

    ADDED: jdbc driver loaded

--- lib/src/main/java/com/bridgelabz/employeepayroll/DBDemo.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/DBDemo.java
@@ -5,7 +5,16 @@ import java.sql.Connection;
 public class DBDemo {
 	
 	public static void main(String[] args) {
-		
+		String jdbcURL="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName="root";
+		String password="abcd1234";
+		Connection con;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		}catch(ClassNotFoundException e) {
+			throw new IllegalStateException("lol",e);
+		}
 	}
 
 }
