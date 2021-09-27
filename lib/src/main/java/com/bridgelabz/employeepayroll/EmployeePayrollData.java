aded4845f35f17cffa3d2cf78e7ef45b394049f9
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-27 15:21:54 +0530

    ADDED: startDate in EmployeePayrollData

--- lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
@@ -3,15 +3,22 @@
  */
 package com.bridgelabz.employeepayroll;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
 	public int id;
 	public String name;
 	public double salary;
+	LocalDate startDate;
 	
 	public EmployeePayrollData(int id, String name, Double salary) {
 		this.id=id;
 		this.name=name;
 		this.salary=salary;
+	}
+	public EmployeePayrollData(Integer id, String name, Double salary, LocalDate startDate) {
+		this(id,name,salary);
+		this.startDate = startDate;
 	}
 	
 	public String toString() {
