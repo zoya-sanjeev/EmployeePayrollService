3371aa972036f4bd926c1a9cc27b09b6089a4287
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-27 22:20:40 +0530

    ADDED: custom Exception class EmployeePayrollException

---
+++ lib/src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollException.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollException extends RuntimeException {
+	enum ExceptionType{
+		CONNECTION_FAILED,
+		INVALID_QUERY
+	}
+	
+	ExceptionType exceptionType;
+
+    public EmployeePayrollException(ExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+
+}
