43448f3e209c1e3aac71fe4ec621348ccba8ce97
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-28 21:50:48 +0530

    ADDED: PayrollDetails class created

--- lib/src/main/java/com/bridgelabz/employeepayroll/PayrollDetails.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/PayrollDetails.java
@@ -1,5 +1,48 @@
 package com.bridgelabz.employeepayroll;
 
 public class PayrollDetails {
+	int employeeId;
+	double basicPay;
+	double deductions;
+	double taxablePay;
+	double tax;
+	double netPay;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public double getBasicPay() {
+		return basicPay;
+	}
+	public void setBasicPay(double basicPay) {
+		this.basicPay = basicPay;
+	}
+	public double getDeductions() {
+		return deductions;
+	}
+	public void setDeductions(double deductions) {
+		this.deductions = deductions;
+	}
+	public double getTaxablePay() {
+		return taxablePay;
+	}
+	public void setTaxablePay(double taxablePay) {
+		this.taxablePay = taxablePay;
+	}
+	public double getTax() {
+		return tax;
+	}
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	public double getNetPay() {
+		return netPay;
+	}
+	public void setNetPay(double netPay) {
+		this.netPay = netPay;
+	}
+	
 
 }
