4ee88186da6fad3c1327c706f0598fdba3177d23
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-28 21:44:29 +0530

    ADDED: created class Address

---
+++ lib/src/main/java/com/bridgelabz/employeepayroll/Address.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.employeepayroll;
+
+public class Address {
+	String address;
+	String city;
+	String state;
+	long zipCode;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public long getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(long zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+}
