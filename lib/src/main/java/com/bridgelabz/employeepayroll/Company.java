7bf86c8733f354a3924a960c472aa7e1590d0445
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-28 21:42:54 +0530

    ADDED: company class created

---
+++ lib/src/main/java/com/bridgelabz/employeepayroll/Company.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.employeepayroll;
+
+public class Company {
+	int companyId;
+	String companyName;
+	
+		public int getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+
+}
